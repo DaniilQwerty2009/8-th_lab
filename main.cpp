9ccdf46945e578f98a21a5c7076bc1030647b89d
Daniil <goltdaniil@gmail.com>
2026-01-08 18:49:21 +0700

    list ready

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <iomanip>
+#include <windows.h>
+
 #include <list>
 #include <iterator>
+
+#include <time.h>
+#include <stdlib.h>
 
 using std::cout;
 using std::endl;
@@ -8,44 +14,139 @@ using std::endl;
 using std::list;
 
 // почему контейнер, decltype
+// добавление, удаление, поиск, сортировка
 
 int main()
 {
-    list<int> L;
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
 
-    for(int i = 1; i <= 10; ++i)
-        L.push_back(i);
+    cout << std::setfill('=') << std::setw(60) << '=' << endl;
+    cout << "ЛАБОРАТОРНАЯ РАБОТА №8" << endl; 
+    cout << "БИБИЛИОТЕА ШАБЛОННЫХ КЛАССОВ STL" << endl;
+    cout << std::setfill('=') <<std::setw(60) << '=' << endl;
+
+    // переменные-счетчики времени
+    time_t start;
+    time_t finish;
+    time_t result;
+
+    // уникальные значения для rand()
+    srand(time(NULL));
 
 
-    for(auto i = L.begin(); i != L.end(); ++i)
-        cout << *i << ' ';
-    cout << endl;
+    cout << "Создадим список случайных положительных чисел длинной 100000000 элементов." << endl;
 
-    L.pop_back();
-    L.pop_front();
+    size_t containerLens = 1000000;
+    list<int> ourList;
 
-    for(auto i = L.begin(); i != L.end(); ++i)
-        cout << *i << ' ';    
-    cout << endl;
+    // список из 100000000 случайных элементов в пределах int-допустимых значений
+    for(int i = 1; i <= containerLens; ++i)
+        ourList.push_back((rand() % INT_MAX - 1) + 1);
 
-    L.insert(++L.begin(), 99);
+    
+    list<int>::iterator iter = ourList.begin();
+    for(int i = 0; i < 7; ++i, ++iter)
+    {
+        cout << *iter << " "; 
+    }
+    cout << "..." << endl;
+    
+    // Засекаем время начала работы алгоритма
+    start = time(NULL);
 
-    for(auto i = L.begin(); i != L.end(); ++i)
-        cout << *i << ' ';    
-    cout << endl;
+    cout << std::setfill('-') <<std::setw(60) << '-' << endl;
 
-    L.erase(--L.end());
+    cout << "Удалим и добавим элементы в начало, середину и конец списка." << endl;
+    cout << "После, отсортируем список и найдем три добавленных элемента" << endl;
 
-    for(auto i = L.begin(); i != L.end(); ++i)
-        cout << *i << ' ';    
-    cout << endl;
+    cout << std::setfill('-') <<std::setw(60) << '-' << endl;
 
-    L.sort();
+    cout << "Удаление первого, среднего и последнего элемента..." << endl;
 
-    for(auto i = L.begin(); i != L.end(); ++i)
-        cout << *i << ' ';    
-    cout << endl;
+    // 1-го
+    ourList.pop_front(); 
+    containerLens--;
 
+    // среднего
+    iter = ourList.begin();
+    // перемещаем итератор до середины списка
+    for(int i = 1; i <= containerLens/2; ++i)
+        iter++;
+    // удаляем с середины
+    ourList.erase(iter);
+    containerLens--;
+    
+
+    // удаление последнего
+    ourList.pop_back();
+    containerLens--;
+
+    cout << std::setfill('-') <<std::setw(60) << '-' << endl;
+
+    cout << "Добавление первого, среднего и последнего элемента..." << endl;
+  
+    // в начало
+    int pushedFront = rand() % INT_MAX;
+    cout << "Первый элемент:" << pushedFront << endl;
+    ourList.push_front(pushedFront);
+    containerLens++;
+    
+
+    // в середину
+    iter = ourList.begin();
+    // перемещаем итератор до середины списка
+    for(int i = 1; i <= containerLens/2; ++i)
+        iter++;
+
+    int pushedMidle = rand() % INT_MAX;
+    cout << "Средний элемент:" << pushedMidle << endl;
+    ourList.insert(iter, pushedMidle);
+    containerLens++;
+
+
+    // в конец
+    int pushedBack = rand() % INT_MAX;
+    cout << "Последний элемент:" << pushedBack << endl;
+    ourList.push_back(pushedBack);
+    containerLens++;
+    
+
+    cout << std::setfill('-') <<std::setw(60) << '-' << endl;
+
+    cout << "Сортировка списка..." << endl;
+    ourList.sort();
+
+    iter = ourList.begin();
+    for(int i = 0; i < 7; ++i, ++iter)
+    {
+        cout << *iter << " "; 
+    }
+    cout << "..." << endl;
+
+
+    cout << std::setfill('-') <<std::setw(60) << '-' << endl;
+
+    cout << "Поиск трех добавленных элементов..." << endl;
+
+    iter = ourList.begin();
+
+    cout << "Найденны элементы: " << endl;
+    for(int findedElementsCounter = 0; iter != ourList.end() && findedElementsCounter != 3; ++iter)
+    {
+        if(*iter == pushedFront || *iter == pushedMidle || *iter == pushedBack)
+        {
+            cout << *iter << endl;
+            findedElementsCounter++;
+        }
+    }
+
+
+    finish = time(NULL);
+    result = finish - start;
+    cout << result << endl;
+
+    
     
     return 0;
 }
